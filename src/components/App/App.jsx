5ccd0e93d1e40adad50bb453ef40e08ea843dcdd
KandaurovF <infinityride86@gmail.com>
2023-08-11 20:25:34 +0300

    refartor whis React-hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppSheet } from './App.styled';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,51 +8,53 @@ import { Button } from '../Button/Button';
 import { Loader } from '../Loader/Loader';
 import * as API from 'services/api';
 
-export default class App extends Component {
-  state = {
-    searchQuery: '',
-    page: 1,
-    hits: [],
-    isLoading: false,
-    isError: false,
-    BtnLoadMore: false,
-  };
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [hits, setHits] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [error, setError] = useState(null);
 
-  fetchData = async () => {
-    const { searchQuery, page } = this.state;
-    this.setState({ isLoading: true });
-    try {
-      const data = await API.fetchAllData(searchQuery, page);
-      const totalPages = Math.ceil(data.totalHits / 12);
-      if (data.hits.length === 0) {
-        toast.error(
-          'Sorry, there are no images matching to your search query!'
-        );
-        return;
+  useEffect(() => {
+    if (searchQuery === '') return;
+
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const data = await API.fetchAllData(searchQuery, page);
+        const calculatedTotalPages = Math.ceil(data.totalHits / 12);
+        setTotalPages(calculatedTotalPages);
+
+        if (data.hits.length === 0) {
+          toast.error(
+            'Sorry, there are no images matching to your search query!'
+          );
+          return;
+        }
+
+        setHits(prevHits => [...prevHits, ...data.hits]);
+
+        if (page === 1) {
+          toast.success(`We found ${data.totalHits} images!`);
+        } else {
+          setTimeout(() => scroll(), 100);
+        }
+
+        if (page >= calculatedTotalPages) {
+          toast.info('Sorry, there is no matches!');
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
       }
+    };
 
-      this.setState(({ hits }) => ({
-        hits: [...hits, ...data.hits],
-        totalPages,
-      }));
+    fetchData();
+  }, [searchQuery, page]);
 
-      if (page === 1) {
-        toast.success(`We found ${data.totalHits} images!`);
-      } else {
-        setTimeout(() => this.scroll(), 100);
-      }
-
-      if (page >= totalPages) {
-        toast.info('Sorry, there is no matches!');
-      }
-    } catch (error) {
-      this.setState({ isError: true, Error });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-
-  scroll = () => {
+  const scroll = () => {
     const { clientHeight } = document.documentElement;
     window.scrollBy({
       top: clientHeight - 180,
@@ -60,39 +62,32 @@ export default class App extends Component {
     });
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { searchQuery, page } = this.state;
-    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
-      this.fetchData();
-    }
-  }
-
-  handleSearchQuery = searchQuery => {
-    this.setState({ searchQuery, page: 1, hits: [] });
+  const handleSearchQuery = newSearchQuery => {
+    setSearchQuery(newSearchQuery);
+    setPage(1);
+    setHits([]);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { hits, isLoading, page, totalPages } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSearchQuery} />
-        <AppSheet>
-          {hits.length !== 0 && <CreateGallery hits={hits} />}
-          {isLoading ? (
-            <Loader />
-          ) : (
-            page < totalPages &&
-            hits.length !== 0 && <Button onLoadMore={this.handleLoadMore} />
-          )}
-        </AppSheet>
-        <ToastContainer autoClose={3000} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Searchbar onSubmit={handleSearchQuery} />
+      <AppSheet>
+        {hits.length !== 0 && <CreateGallery hits={hits} />}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          page < totalPages &&
+          hits.length !== 0 && <Button onLoadMore={handleLoadMore} />
+        )}
+        {error && <p>An error occurred: {error.message}</p>}
+      </AppSheet>
+      <ToastContainer autoClose={3000} />
+    </>
+  );
+};
+
+export default App;
